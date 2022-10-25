2c3fd7c9700315b35b6a3c51383a5e7823bec601
AZ <antonzhuikov@gmail.com>
2022-10-25 22:42:40 +0400

    Update FundMe.sol

--- FundMe.sol
+++ FundMe.sol
@@ -3,22 +3,29 @@ pragma solidity ^0.8.0;
 
 import "./PriceConvertor.sol";
 
+error NotOwner();
+error NotEnough();
+
 contract FundMe {
 
     using PriceConvertor for uint256;
-    
 
-    uint256 internal minimumUsd = 50 * 1e18;
+    uint256 internal constant MINIMUM_USD = 50 * 1e18;
     address[] funders;
     mapping(address => uint256) addressToAmountFunded;
 
-    function fund() public payable {
-        require(msg.value.getConvertionRate() >= minimumUsd, "Didn't send enough!");
+    address internal immutable i_owner;
+
+    constructor() {
+        i_owner = msg.sender;
+    }
+
+    function fund() public payable moreThenMinimumFunds {
         funders.push(msg.sender);
         addressToAmountFunded[msg.sender] += msg.value;
     }
 
-    function withdraw() public {
+    function withdraw() public onlyOwner {
         for (uint256 i = 0; i < funders.length; i++) {
             address funder = funders[i];
             addressToAmountFunded[funder] = 0;
@@ -27,5 +34,23 @@ contract FundMe {
 
         (bool callSuccess,) = payable(msg.sender).call{ value: address(this).balance }("");
         require(callSuccess, "Withdraw failed");
+    }
+
+    modifier onlyOwner {
+        if (msg.sender != i_owner) { revert NotOwner(); }
+        _;
+    }
+
+    modifier moreThenMinimumFunds {
+        if (msg.value.getConvertionRate() < MINIMUM_USD) { revert NotEnough(); }
+        _;
+    }
+
+    receive() external payable {
+        fund();
+    }
+
+    fallback() external payable {
+        fund();
     }
 }
